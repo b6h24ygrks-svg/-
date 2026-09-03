aea626d77ef873922a4daa7190b0530b96d91f7e
b6h24ygrks-svg <b6h24ygrks@privaterelay.appleid.com>
2026-09-03 12:59:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const SUPABASE_URL = "https://wpdlcnniaukqqiepvwnt.supabase.co";
+const SUPABASE_KEY = "sb_publishable_2b-gDkZYM8RgtWk8xnu0Jw_pJ0JWzM_;
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const SECTIONS=[
   {title:"💗 關於我",qs:[
     "最近半年都叫我什麼？",
